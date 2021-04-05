2ea1b0ecc851a0c2d3ac10c2a0d229f79ae7131a
vidya.vithayathi <vidya.vithayathil@kineticsoftware.com>
2021-04-05 14:11:25 +0100

    Scaffolded for Booking Service Tests

---
+++ PDR.PatientBooking.Service.Tests/BookingServices/BookingServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using PDR.PatientBooking.Data;
+using PDR.PatientBooking.Data.Models;
+using PDR.PatientBooking.Service.ClinicServices;
+using PDR.PatientBooking.Service.ClinicServices.Requests;
+using PDR.PatientBooking.Service.ClinicServices.Responses;
+using PDR.PatientBooking.Service.ClinicServices.Validation;
+using PDR.PatientBooking.Service.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PDR.PatientBooking.Service.Tests.BookingServices
+{
+    public class BookingServiceTests
+    {
+    }
+}
